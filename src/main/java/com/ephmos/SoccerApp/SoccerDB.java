e97d1e025ed1926d60f6983d3fdbf6db1de09eea
Ephmos <arefokmido@protonmail.com>
2025-11-28 10:50:27 +0100

    Eliminación de las antiguas implementaciones de la interfaz, tanto en las propias clases como en el main de la aplicación.

--- src/main/java/com/ephmos/SoccerApp/SoccerDB.java
+++ src/main/java/com/ephmos/SoccerApp/SoccerDB.java
@@ -7,81 +7,98 @@ import com.ephmos.SoccerApp.others.Positions;
 import java.util.TreeSet;
 
 public class SoccerDB implements SoccerDAO {
+    // Para insertar un jugador en la base de datos.
     @Override
     public void createPlayer(Player player) {
-        
+
     }
 
+    // Para eliminar un jugador de la base de datos.
     @Override
     public void deletePlayer(Player player) {
 
     }
 
+    // Para actualizar un jugador en la base de datos.
     @Override
     public void updatePlayer(Player player) {
 
     }
 
+    // Para comprobar si un jugador existe en la base de datos.
     @Override
     public boolean playerExists(Player player) {
         return false;
     }
 
+    // TERMINAR: Pensar en la lógica interna del método para realizar una comprobación específica.
+    // Para localizar un jugador por los parámetros especificados, en caso de no especificar uno no se tendrá en cuenta.
     @Override
     public TreeSet<Player> findPlayer(String name, String lastname, String team, int age, Positions position, int goalsNumber) {
         return null;
     }
 
+    // Leer todos los jugadores
     @Override
     public TreeSet<Player> readAllPlayers() {
         return null;
     }
 
+    // Buscar el top goleadores
     @Override
     public TreeSet<Player> findTopScorer() {
         return null;
     }
 
+    // Buscar el top goleadores por equipo
     @Override
     public TreeSet<Player> findTopScorer(String team) {
         return null;
     }
 
+    // Ordenar por nombre
     @Override
     public TreeSet<Player> sortByName() {
         return null;
     }
 
+    // Ordenar por apellido
     @Override
     public TreeSet<Player> sortByLastname() {
         return null;
     }
 
+    // Ordenar por edad
     @Override
     public TreeSet<Player> sortByAge(boolean ascending) {
         return null;
     }
 
+    // Ordenar por posición
     @Override
     public TreeSet<Player> sortByPosition() {
         return null;
     }
 
+    // Ordenar por goles
     @Override
     public TreeSet<Player> sortByGoals(boolean ascending) {
         return null;
     }
 
+    // Ordenar por equipo
     @Override
     public TreeSet<Player> sortByTeam() {
         return null;
     }
 
+    // Obtener la media de edad
     @Override
     public double getAverageAge() {
         return 0;
     }
 
+    // Obtener la media de goles
     @Override
     public double getAverageGoals() {
         return 0;
